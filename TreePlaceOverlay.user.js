caa3628ecdbbb3c3e602b1babd7fb57add3306e9
Questlog <Questlog@github.com>
2022-04-02 23:29:26 +0200

    init

---
+++ TreePlaceOverlay.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Tree Place Overlay
+// @namespace    http://tampermonkey.net/
+// @version      3.0
+// @description  Keep the canvas beautiful!
+// @author       oralekin from osu! /r/osuplace
+// @match        https://hot-potato.reddit.com/embed*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @updateURL    https://git.o9p0.de/trent/Trees/raw/branch/master/TreePlaceOverlay.user.js
+// @downloadURL  https://git.o9p0.de/trent/Trees/raw/branch/master/TreePlaceOverlay.user.js
+// @grant        none
+// ==/UserScript==
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+        (function () {
+            const i = document.createElement("img");
+            i.src = "https://git.o9p0.de/trent/Trees/raw/branch/master/treeoverlay.png";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated; image-rendering: -moz-crisp-edges;width: 2000pxheight: 1000px;";
+            console.log(i);
+            return i;
+        })())
+
+    }, false);
+
+}
